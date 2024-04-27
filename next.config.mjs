1898f0e1deee7a076d815e845682c21bf444f9ad
Olusola Olagunju <olusola.olagunju@shortletapp.com>
2024-04-27 15:46:54 +0100

    feat: firebase setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export",
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
